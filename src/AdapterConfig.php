cd04dc9836a282be22edd4130ddf7e3aab6f4e59
Cameron Van Orman <cameron.vanorman@gmail.com>
2024-06-05 13:05:42 -0700

    Add abstract config class

---
+++ src/AdapterConfig.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NortheasternWeb\PIMFIMAdapter;
+
+abstract class AdapterConfig {
+
+    public string $space_id;
+    public string $access_token;
+    public string $environment_id;
+
+    public function __construct() {
+        $this->space_id = $this->getSpaceId();
+        $this->access_token = $this->getAccessToken();
+        $this->environment_id = $this->getEnvironmentId();
+
+        return [
+            'space_id' => $this->space_id,
+            'access_token' => $this->access_token,
+            'environment_id' => $this->environment_id
+        ];
+    }
+
+    public function getSpaceId() {}
+
+    public function getAccessToken() {}
+
+    public function getEnvironmentId() {}
+}
