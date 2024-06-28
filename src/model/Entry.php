7cbeda4123bc7880342b7c98835e0c082b2b8446
Cameron Van Orman <cameron.vanorman@gmail.com>
2024-06-28 13:07:05 -0700

    Add generic Entry model class

---
+++ src/model/Entry.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace NortheasternWeb\PIMFIMAdapter\Model;
+
+class Entry {
+    protected $id;
+    protected $createdAt;
+    protected $updatedAt;
+    protected $revision;
+
+    public function __construct($item) {
+        $this->id = $item->getId();
+        $this->createdAt = $item->getCreatedAt();
+        $this->updatedAt = $item->getUpdatedAt();
+        $this->revision = $item->getRevision();
+    }
+
+    public function toArray() {
+        return [
+            'createdAt' => $this->createdAt,
+            'updatedAt' => $this->updatedAt,
+            'revision' => $this->revision,
+        ];
+    }
+}
