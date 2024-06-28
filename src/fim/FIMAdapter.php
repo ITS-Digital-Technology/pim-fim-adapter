6a6ea527f3a24b35c3da4123654134680c96c075
Cameron Van Orman <cameron.vanorman@gmail.com>
2024-06-28 14:41:27 -0700

    Update FIMAdapter with optional ClientOptions parameter

--- src/fim/FIMAdapter.php
+++ src/fim/FIMAdapter.php
@@ -4,18 +4,18 @@ namespace NortheasternWeb\PIMFIMAdapter\FIM;
 
 use NortheasternWeb\PIMFIMAdapter\FIM\Config\FIMConfig;
 use NortheasternWeb\PIMFIMAdapter\ContentfulAdapter;
-use Contentful\Delivery\ClientOptions;
 use NortheasternWeb\PIMFIMAdapter\Adapter;
+use Contentful\Delivery\ClientOptions;
 
 class FIMAdapter extends Adapter {
     private FIMConfig $config;
     private ContentfulAdapter $adapter;
 
     function __construct(
-        ClientOptions $client_options = null
+        ?ClientOptions $client_options = null
     ) {
         $this->config = new FIMConfig();
 
-        return $this->adapter = parent::__construct($this->config);
+        return $this->adapter = parent::__construct($this->config, $client_options);
     }
 }
