4debf7e9725dac8a5a24e990b3614611a2ce9bf6
Cameron Van Orman <cameron.vanorman@gmail.com>
2024-07-01 11:54:26 -0700

    Add index.php

---
+++ tests/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use Contentful\Core\Resource\ResourceInterface;
+use Contentful\Delivery\Query;
+use Contentful\Delivery\Resource\Entry;
+use Dotenv\Dotenv;
+use NortheasternWeb\PIMFIMAdapter\ContentfulAdapter;
+use NortheasternWeb\PIMFIMAdapter\FIM\FIMAdapter;
+use NortheasternWeb\PIMFIMAdapter\PIM\PIMAdapter;
+use NortheasternWeb\PIMFIMAdapter\PIM\Model\Program;
+
+require_once realpath('./vendor/autoload.php');
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+// $FIMAdapter = new FIMAdapter($FIMConfig, null);
+
+$PIMAdapter = new PIMAdapter();
+
+// $programs = $PIMAdapter->getAllPrograms();
+// $programsByLocationToronto = $PIMAdapter->getProgramsByLocationName('toronto');
+$programsByLocationBoston = $PIMAdapter->getProgramsByLocationName('boston');
+
+// $programsByMajor = $PIMAdapter->getProgramsByMajorName('Speech-Language Pathology');
+
+$collegeQuery = (new Query())->select(['fields.name', 'sys.id', 'sys.updatedAt']);
+$colleges = $PIMAdapter->getCollegeList($collegeQuery);
+
+$bouveCollege = $PIMAdapter->getProgramsByCollege("Bouvé College of Health Sciences", (new Query)->orderBy('fields.name'));
+
+$programCollection = collect($programsByLocationBoston)->map(function($item) {
+    return (new Program($item))->toArray();
+});
+
+var_dump(json_encode($programCollection, JSON_PRETTY_PRINT));
